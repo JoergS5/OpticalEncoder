f0a6fbb3fb606be40aff451652a0f9025988f77b
JoergS5 <joerg.schnur@outlook.com>
2021-08-31 21:46:56 +0200

    Update MainCreateLinearEncoder.java

--- MainCreateLinearEncoder.java
+++ MainCreateLinearEncoder.java
@@ -2,8 +2,6 @@ package com.joergs5;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Arc2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,66 +9,77 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class MainCreateLinearEncoder {
-	private boolean UP = true;
-	private boolean DOWN = false;
-
+	
 	public static void main(String[] args) {
 		new MainCreateLinearEncoder();
 	}
 
 	public MainCreateLinearEncoder() {
 		int width = 150;		// 600 with 600 dpi => 2.6 cm
-		int height = 6000;	// 600 dpi 6000 => 26 cm (DIN A4, US Letter long, partly printable)
+		int maxvalue = 2350;
+		int minvalue = -2350;
 
-		drawScale(width, height, "opticalencoderlinearLeft", DOWN);
-		drawScale(width, height, "opticalencoderlinearRight", UP);
+		drawScale(width, maxvalue, minvalue, "LinearEncoder600dpi");
 	}
 
-	private void drawScale(int width, int height, String filename, boolean direction) {
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
+	private void drawScale(int width, int maxvalue, int minvalue, String filename) {
+		int height = maxvalue + 1 - minvalue;
+		
+		int bitLength = getBitLength(maxvalue);
+        int thicknessOfBit = width / (bitLength + 6);
+        int imageWidth = (bitLength + 6) * thicknessOfBit;
+		
+		BufferedImage image = new BufferedImage(imageWidth, height, BufferedImage.TYPE_BYTE_BINARY);
         Graphics2D g2d = image.createGraphics();
 
         g2d.setColor(Color.white);
-        g2d.fillRect(0, 0, width-1, height-1);
+        g2d.fillRect(0, 0, imageWidth, height);
 
         GrayCode gc = new GrayCode();
-        int bitLength = getBitLength(height);
-        int thicknessOfBit = width / (bitLength + 4);
         
 		g2d.setColor(Color.black);
-		g2d.fillRect(0, 0, thicknessOfBit, height-1);
+		g2d.fillRect(0, 0, thicknessOfBit, height);
         
 		g2d.setColor(Color.black);
-		g2d.fillRect((bitLength + 3) * thicknessOfBit, 0, thicknessOfBit, height-1);
-        
-        for(int i=0; i < height; i++) {
-        	int graycode = gc.getBinaryToGray(i);
-        	for(int j= 0; j < bitLength; j++) {
-				int value = graycode & (1 << j);
-				if(value > 0) {
-					int x1 = thicknessOfBit * j + 2 * thicknessOfBit;
-					int y1 = 0;
-					if(direction == UP) {
-						y1 = i;
-					}
-					else {
-						y1 = height - i;
-					}
-					g2d.setColor(Color.black);
-					g2d.drawLine(x1, y1, x1 + thicknessOfBit, y1);
-				}
-        	}
-        }
-        
-        
-        
-        
+		g2d.fillRect((bitLength + 5) * thicknessOfBit, 0, thicknessOfBit, height);
+		
+		int currow = 0;
+		for(int value = maxvalue; value >= 0; value--) {
+			drawGrayCode(gc, currow, bitLength, thicknessOfBit, g2d, true, value);
+			currow++;
+		}
+		int ctmin = - minvalue;
+		for(int value = 1; value <= ctmin; value++) {
+			drawGrayCode(gc, currow, bitLength, thicknessOfBit, g2d, false, value);
+			currow++;
+		}
+		
 		File output = new File("c:\\_\\" + filename + ".png");
 	    try {
 			ImageIO.write(image, "png", output);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void drawGrayCode(GrayCode gc, int row, int bitLength, int thicknessOfBit, Graphics2D g2d,
+				boolean positive, int value) {
+    	int graycode = gc.getBinaryToGray(value);
+    	for(int j= 0; j < bitLength; j++) {
+			int grayValue = graycode & (1 << j);
+			if(grayValue > 0) {
+				int x1 = (j + 2 ) * thicknessOfBit;
+				g2d.setColor(Color.black);
+				g2d.drawLine(x1, row, x1 + thicknessOfBit-1, row);
+			}
+    	}
+    	
+    	// add plus marker
+    	if(positive) {
+    		g2d.setColor(Color.black);
+    		int x = (bitLength + 3) * thicknessOfBit;
+    		g2d.drawLine(x, row, x + thicknessOfBit - 1, row);
+    	}
 	}
 	
 	private int getBitLength(int maxNumber) {
